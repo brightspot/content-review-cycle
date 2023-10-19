6571e83a174fd11101f097989df981cfd94a2189
Max Safo <max.safo@brightspot.com>
2023-10-18 21:37:24 -0400

    Add linebreaks for banner

--- src/main/java/brightspot/reviewcycle/ReviewCycleDueBanner.java
+++ src/main/java/brightspot/reviewcycle/ReviewCycleDueBanner.java
@@ -122,6 +122,8 @@ public class ReviewCycleDueBanner implements EditTopHtml {
 
         try {
             page.write(bannerMessage);
+            page.writeStart("br");
+            page.writeStart("br");
         } catch (IOException e) {
             LOGGER.warn("Error while writing embargo banner!", e);
         }
