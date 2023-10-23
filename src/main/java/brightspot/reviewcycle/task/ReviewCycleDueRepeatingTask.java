1c49e062678227d2b7312e0197fd652cc00b6f46
Max Safo <max.safo@brightspot.com>
2023-10-23 11:52:06 -0400

    Set repeating task to once per day and add comment

--- src/main/java/brightspot/reviewcycle/task/ReviewCycleDueRepeatingTask.java
+++ src/main/java/brightspot/reviewcycle/task/ReviewCycleDueRepeatingTask.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 /**
  * This ReviewCycleDueRepeatingTask publishes notifications of content that is due or has a warning, both overriden
  * and non-overriden content. This task is set to repeat once per day.
+ * The method {@link #calculateRunTime(DateTime)} sets how often this task should be repeated.
  */
 public class ReviewCycleDueRepeatingTask extends RepeatingTask {
 
@@ -173,7 +174,7 @@ public class ReviewCycleDueRepeatingTask extends RepeatingTask {
 
     @Override
     protected DateTime calculateRunTime(DateTime currentTime) {
-        return everyMinute(currentTime);
+        return everyDay(currentTime);
     }
 
 }
