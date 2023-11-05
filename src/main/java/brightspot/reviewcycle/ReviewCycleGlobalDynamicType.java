1b5d2c550b90c5bdf0c9ffe135492e5f1162b07e
Max Safo <max.safo@brightspot.com>
2023-11-05 18:19:03 -0500

    If content type not configured in sites & settings, hide fields in review cycle modification

--- src/main/java/brightspot/reviewcycle/ReviewCycleGlobalDynamicType.java
+++ src/main/java/brightspot/reviewcycle/ReviewCycleGlobalDynamicType.java
@@ -1,10 +1,16 @@
 package brightspot.reviewcycle;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.psddev.cms.db.Site;
+import com.psddev.cms.db.SiteSettings;
 import com.psddev.cms.db.ToolUi;
+import com.psddev.cms.ui.ToolRequest;
 import com.psddev.cms.ui.form.DynamicType;
 import com.psddev.dari.db.ObjectType;
 import com.psddev.dari.db.Recordable;
+import com.psddev.dari.web.WebRequest;
 
 /**
  * ReviewCycleGlobalDynamicType hides review cycle controls on global content
@@ -23,6 +29,28 @@ public class ReviewCycleGlobalDynamicType implements DynamicType {
                 .filter(objectField -> objectField.getInternalName()
                     .startsWith(ReviewCycleContentModification.FIELD_PREFIX))
                 .forEach(field -> field.as(ToolUi.class).setHidden(true));
+        }
+
+        Site site = WebRequest.getCurrent().as(ToolRequest.class).getCurrentSite();
+
+        // Get content type maps lists from sites & settings
+        List<ReviewCycleContentTypeMap> mapsList = SiteSettings.get(
+                site,
+                s -> s.as(ReviewCycleSiteSettings.class).getSettings().getContentTypeMaps());
+
+        // Get the content type maps into a list of object types
+        List<ObjectType> reviewCycleContentTypeMapObjectList
+                = mapsList
+                .stream()
+                .map(ReviewCycleContentTypeMap::getContentType)
+                .collect(Collectors.toList());
+
+        // If true, then we know that sites & settings content type map list does not have the object configured
+        if (!reviewCycleContentTypeMapObjectList.contains(type)) {
+            type.getFields().stream()
+                    .filter(objectField -> objectField.getInternalName()
+                            .startsWith(ReviewCycleContentModification.FIELD_PREFIX))
+                    .forEach(field -> field.as(ToolUi.class).setHidden(true));
         }
     }
 }
